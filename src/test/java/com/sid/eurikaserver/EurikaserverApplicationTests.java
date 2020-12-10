8da7d52ed2c8823ad989a25672ed18ac71abd5a0
jinzo <aminjimzo@gmail.com>
2020-12-10 20:29:25 +0100

    Initial commit

---
+++ src/test/java/com/sid/eurikaserver/EurikaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.eurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
